b5a94858da53431c432b71fd7060c77eac80fc1a
marionatuhwo <ano@hawaii.edu>
2022-09-21 08:21:05 -1000

    Commit String Interpolation example

---
+++ stringInterpolation.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//(Source: Apple Documentation) String Interpolation is a way to construct a new String value from a mix of constants, variables, literals, expressions by including their values inside a string literal. Each item that you insert into the string literal is wrapped in a pair of parentheses prefixed by a backslash \
+
+//Let's run through an example:
+
+var personName = "Tom"
+
+//string interpolation example
+
+let greeting = "Hi, my name is \(personName)"
+
+
+
